943f9ba40083d76623d08695a5442c388b748122
Evan Jeffries <ejeffries14@gmail.com>
2019-07-12 06:54:02 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -14,8 +14,7 @@ end
 def age_appropriate_birthday(birthday_kids)
   
   birthday_kids.each do |kids_name,age|
-    if age <= 12
     else
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
-
+end
